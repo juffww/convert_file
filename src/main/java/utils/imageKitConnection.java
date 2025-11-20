5f40df3ef72690343af98a399c5e42dd395c720e
juffww <baoho1503@gmail.com>
2025-11-21 03:18:37 +0700

    Them ImageKit connection va sua pom.xml

---
+++ src/main/java/utils/imageKitConnection.java
@@ -0,0 +1,23 @@
+package utils;
+
+import io.imagekit.sdk.ImageKit;
+import io.imagekit.sdk.config.Configuration;
+import io.imagekit.sdk.utils.Utils;
+
+public class imageKitConnection {
+
+    private static ImageKit imageKit;
+
+    public static ImageKit getInstance() {
+        if (imageKit == null) {
+            imageKit = ImageKit.getInstance();
+            Configuration config = new Configuration(
+                    "public_jKU5hzTx8Xk7etZmYccARVn4Mok=",
+                    "private_3PeBvD74MsvDfL7a1/+xUURsniM=",
+                    "https://ik.imagekit.io/s3bxwjz5c"
+            );
+            imageKit.setConfig(config);
+        }
+        return imageKit;
+    }
+}
